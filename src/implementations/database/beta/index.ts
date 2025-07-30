17bcd1783e7e7108fba358901a4747e87a62b6f3
Glastis <15161394+Glastis@users.noreply.github.com>
2025-07-30 11:27:54 +0200

    fix: implementInterface no longer throw an error (#6)
    * fix: implementInterface no longer throw an error due to its async removal
    
    * chore: await missing in runQuery and closeCursor

--- src/implementations/database/beta/index.ts
+++ src/implementations/database/beta/index.ts
@@ -1101,14 +1101,16 @@ modes.dbList = () => {
 };
 
 export namespace internal {
-  export function runQuery(query: internalRuntime.QueryBuilderContext) {
+  export async function runQuery(query: internalRuntime.QueryBuilderContext) {
     // TODO: make sure return values follow the typing
     const dbQuery = processQuery(query, {
       vars: [],
       args: {},
     });
     console.log('runQuery', JSON.stringify(dbQuery));
-    if (dbQuery.mode in modes) return modes[dbQuery.mode](dbQuery);
+    if (dbQuery.mode in modes) {
+      return await modes[dbQuery.mode](dbQuery);
+    }
   }
 
   const openedCursors = new Map<number, [AggregationCursor, AsyncIterableIterator<any>, boolean]>();
@@ -1128,15 +1130,17 @@ export namespace internal {
     }
     const [, iterator, single_val] = openedCursors.get(reqId)!;
     let res = await iterator.next();
-    if (single_val && typeof res.value === 'object') res.value = res.value.__singleval;
+    if (single_val && typeof res.value === 'object') {
+      res.value = res.value.__singleval;
+    }
     return res;
   }
 
-  export function closeCursor(reqId: number) {
+  export async function closeCursor(reqId: number) {
     if (openedCursors.has(reqId)) {
       const [cursor] = openedCursors.get(reqId)!;
       openedCursors.delete(reqId);
-      return cursor.close();
+      return await cursor.close();
     }
   }
 }
