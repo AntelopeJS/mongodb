3000cc45b8b42c62ada0104edd07e1354536beb0
Thomasims <thomas@antelopejs.com>
2026-03-03 13:34:38 +0100

    feat: use Selection in lookup instead of Table

--- src/interfaces/database/beta/datum.ts
+++ src/interfaces/database/beta/datum.ts
@@ -1,6 +1,6 @@
 import { ExtractType, Value } from './common';
 import { Query } from './query';
-import { Table } from './selection';
+import { Selection } from './selection';
 import { ValueProxy } from './valueproxy';
 
 export class Datum<T> extends Query<T> {
@@ -62,7 +62,7 @@ export class Datum<T> extends Query<T> {
    * @param otherKey Key in other table
    */
   public lookup<U = any, TK extends keyof T = keyof T>(
-    other: Table<U>, // TODO: swap to Stream<U>
+    other: Selection<U>,
     localKey: TK,
     otherKey: keyof U,
   ) {
