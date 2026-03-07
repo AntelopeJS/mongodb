2a6b2320a89d96058e894e632344db011c2284f6
Antony Rizzitelli <upd4ting@gmail.com>
2026-03-07 18:00:24 +0100

    fix: resolve biome lint warnings
    - Add guard for desc.value in CondenseValue decorator
    - Extract this.wrappedObject to local const before closure
    - Replace null! with new DecodingContext() for createInstance/destroyInstance
    - Add biome-ignore for cross-file reassigned let
    - Use type assertion instead of non-null assertion in tests
    - Reset connected flag in Disconnect()

--- src/implementations/database/beta/expression.ts
+++ src/implementations/database/beta/expression.ts
@@ -1,19 +1,24 @@
-import { QueryStage } from '@ajs.local/database/beta/common';
-import { DecodingContext, Temporary } from './utils';
-import { DecodeFunction, DecodeValue } from './query';
+import type { QueryStage } from "@ajs.local/database/beta/common";
+import { DecodeFunction, DecodeValue } from "./query";
+import { type DecodingContext, Temporary } from "./utils";
 
 /**
  * This decorator should be used on stages where this.value is used multiple times.
  */
-function CondenseValue(cl: Expression, key: string, desc: PropertyDescriptor) {
-  const prev = desc.value!;
+function CondenseValue(
+  _cl: Expression,
+  _key: string,
+  desc: PropertyDescriptor,
+) {
+  const prev = desc.value;
+  if (!prev) return;
   desc.value = function (this: Expression, ...args: any[]) {
     const valueBefore = this.value;
-    if (typeof valueBefore === 'string') {
+    if (typeof valueBefore === "string") {
       return prev.apply(this, args);
     }
     const tmp = Temporary();
-    this.value = '$$' + tmp;
+    this.value = `$$${tmp}`;
     return {
       $let: {
         vars: { [tmp]: valueBefore },
@@ -31,7 +36,11 @@ export class Expression {
     public value?: unknown,
   ) {}
 
-  public static async decode(stages: QueryStage[], context: DecodingContext, startValue?: unknown) {
+  public static async decode(
+    stages: QueryStage[],
+    context: DecodingContext,
+    startValue?: unknown,
+  ) {
     const expr = new Expression(context, startValue);
     await expr.addStages(stages);
     return expr.value;
@@ -41,21 +50,28 @@ export class Expression {
     for (const stage of stages) {
       const stageCallback = `stage_${stage.stage}`;
       if (!(stageCallback in this)) {
-        throw new Error('Unimplemented stage: ' + stage.stage);
+        throw new Error(`Unimplemented stage: ${stage.stage}`);
       }
       const callback = this[stageCallback as keyof this];
-      if (typeof callback === 'string') {
+      if (typeof callback === "string") {
         this.value = {
           [callback]:
             stage.args.length === 0
               ? this.value
-              : [this.value, ...(await Promise.all(stage.args.map((arg) => DecodeValue(arg, this.context))))],
+              : [
+                  this.value,
+                  ...(await Promise.all(
+                    stage.args.map((arg) => DecodeValue(arg, this.context)),
+                  )),
+                ],
         };
       } else if (callback instanceof Function) {
         this.options = stage.options;
         this.value = await callback.apply(
           this,
-          await Promise.all(stage.args.map((arg) => DecodeValue(arg, this.context))),
+          await Promise.all(
+            stage.args.map((arg) => DecodeValue(arg, this.context)),
+          ),
         );
       }
     }
@@ -69,14 +85,14 @@ export class Expression {
     return DecodeValue(constant, this.context);
   }
 
-  stage_default = '$ifNull';
-  stage_and = '$and';
-  stage_or = '$or';
-  stage_not = '$not';
-  stage_eq = '$eq';
-  stage_ne = '$ne';
-  stage_add = '$add';
-  stage_sub = '$subtract';
+  stage_default = "$ifNull";
+  stage_and = "$and";
+  stage_or = "$or";
+  stage_not = "$not";
+  stage_eq = "$eq";
+  stage_ne = "$ne";
+  stage_add = "$add";
+  stage_sub = "$subtract";
 
   @CondenseValue
   stage_date_during(low: unknown, high: unknown) {
@@ -93,50 +109,54 @@ export class Expression {
   @CondenseValue
   stage_date_tod() {
     return {
-      $add: [{ $mul: [{ $hour: this.value }, 3600] }, { $mul: [{ $minute: this.value }, 60] }, { $second: this.value }],
+      $add: [
+        { $mul: [{ $hour: this.value }, 3600] },
+        { $mul: [{ $minute: this.value }, 60] },
+        { $second: this.value },
+      ],
     };
   }
-  stage_date_year = '$year';
-  stage_date_month = '$month';
-  stage_date_day = '$dayOfMonth';
-  stage_date_dow = '$dayOfWeek';
-  stage_date_doy = '$dayOfYear';
-  stage_date_hours = '$hour';
-  stage_date_minutes = '$minute';
-  stage_date_seconds = '$second';
+  stage_date_year = "$year";
+  stage_date_month = "$month";
+  stage_date_day = "$dayOfMonth";
+  stage_date_dow = "$dayOfWeek";
+  stage_date_doy = "$dayOfYear";
+  stage_date_hours = "$hour";
+  stage_date_minutes = "$minute";
+  stage_date_seconds = "$second";
   stage_date_epoch() {
     return { $divide: [{ $toLong: this.value }, 1000] };
   }
 
-  stage_mul = '$multiply';
-  stage_div = '$divide';
-  stage_mod = '$mod';
-  stage_round = '$round';
-  stage_ceil = '$ceil';
-  stage_floor = '$floor';
-  stage_bit_and = '$bitAnd'; // TODO: bitwise operations only work on Int32() and Long(), integrate with schema?
-  stage_bit_or = '$bitOr';
-  stage_bit_xor = '$bitXor';
-  stage_bit_not = '$bitNot';
-  stage_bit_lshift = '$bit_lshift'; // TODO: should we keep bitshifts? { $multiply: [value, { $pow: [2, arg] }], }
-  stage_bit_rshift = '$bit_rshift';
+  stage_mul = "$multiply";
+  stage_div = "$divide";
+  stage_mod = "$mod";
+  stage_round = "$round";
+  stage_ceil = "$ceil";
+  stage_floor = "$floor";
+  stage_bit_and = "$bitAnd"; // TODO: bitwise operations only work on Int32() and Long(), integrate with schema?
+  stage_bit_or = "$bitOr";
+  stage_bit_xor = "$bitXor";
+  stage_bit_not = "$bitNot";
+  stage_bit_lshift = "$bit_lshift"; // TODO: should we keep bitshifts? { $multiply: [value, { $pow: [2, arg] }], }
+  stage_bit_rshift = "$bit_rshift";
 
-  stage_cmp_gt = '$gt';
-  stage_cmp_ge = '$gte';
-  stage_cmp_lt = '$lt';
-  stage_cmp_le = '$lte';
+  stage_cmp_gt = "$gt";
+  stage_cmp_ge = "$gte";
+  stage_cmp_lt = "$lt";
+  stage_cmp_le = "$lte";
 
   stage_str_split() {
-    const split = { $split: [this.value, this.options?.separator ?? ' '] };
+    const split = { $split: [this.value, this.options?.separator ?? " "] };
     if (this.options?.maxSplits) {
       return { $slice: [split, this.options.maxSplits] };
     }
     return split;
   }
-  stage_str_concat = '$add';
-  stage_str_upcase = '$toUpper';
-  stage_str_downcase = '$toLower';
-  stage_str_len = '$strLenCP';
+  stage_str_concat = "$add";
+  stage_str_upcase = "$toUpper";
+  stage_str_downcase = "$toLower";
+  stage_str_len = "$strLenCP";
   stage_str_match(regex: unknown) {
     return {
       $regexMatch: {
@@ -146,7 +166,7 @@ export class Expression {
     };
   }
 
-  stage_arr_index = '$arrayElemAt';
+  stage_arr_index = "$arrayElemAt";
   stage_arr_includes(val: unknown) {
     return { $in: [val, this.value] };
   }
@@ -164,7 +184,7 @@ export class Expression {
       $map: {
         input: this.value,
         as: tmp,
-        in: DecodeFunction(func, this.context, ['$$' + tmp]),
+        in: DecodeFunction(func, this.context, [`$$${tmp}`]),
       },
     };
   }
@@ -174,22 +194,24 @@ export class Expression {
       $filter: {
         input: this.value,
         as: tmp,
-        cond: DecodeFunction(func, this.context, ['$$' + tmp]),
+        cond: DecodeFunction(func, this.context, [`$$${tmp}`]),
       },
     };
   }
   stage_arr_empty() {
     return { $eq: [0, { $size: this.value }] };
   }
-  stage_arr_count = '$size';
-  stage_arr_sum = '$sum';
-  stage_arr_avg = '$avg';
-  stage_arr_min = '$min';
-  stage_arr_max = '$max';
+  stage_arr_count = "$size";
+  stage_arr_sum = "$sum";
+  stage_arr_avg = "$avg";
+  stage_arr_min = "$min";
+  stage_arr_max = "$max";
 
   stage_obj_index(key: string, def: unknown) {
     const index =
-      typeof this.value === 'string' && typeof key === 'string' && this.value.startsWith('$')
+      typeof this.value === "string" &&
+      typeof key === "string" &&
+      this.value.startsWith("$")
         ? `${this.value}.${key}`
         : { $getField: { field: key, input: this.value } };
     if (def) {
@@ -197,13 +219,13 @@ export class Expression {
     }
     return index;
   }
-  stage_obj_merge = '$mergeObjects';
+  stage_obj_merge = "$mergeObjects";
   stage_obj_keys() {
     return {
       $map: {
         input: { $objectToArray: this.value },
-        as: 'temporary_entries',
-        in: '$$temporary_entries.k',
+        as: "temporary_entries",
+        in: "$$temporary_entries.k",
       },
     };
   }
@@ -211,8 +233,8 @@ export class Expression {
     return {
       $map: {
         input: { $objectToArray: this.value },
-        as: 'temporary_entries',
-        in: '$$temporary_entries.v',
+        as: "temporary_entries",
+        in: "$$temporary_entries.v",
       },
     };
   }
